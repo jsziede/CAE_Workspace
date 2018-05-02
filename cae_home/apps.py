e059c63daf5c53d2c44de251fc30d2eaf25106a2
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-05-02 11:45:02 -0400

    Create initial cae_home app

---
+++ cae_home/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CaeHomeConfig(AppConfig):
+    name = 'cae_home'
---
+++ cae_home/urls.py
@@ -0,0 +1,9 @@
+"""
+Urls for CAE_Home App.
+"""
+
+
+app_name = 'cae_home'
+urlpatterns = [
+
+]
