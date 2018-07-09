db7c678a32620af0b356476ac6ef9844255c78a1
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-07-09 18:17:57 -0400

    Add handling for projects having an empty index in settings

--- cae_home/views.py
+++ cae_home/views.py
@@ -1,17 +1,20 @@
 """
 Views for CAE_Home App.
 """
+
 from django.conf import settings
 from django.shortcuts import render
 
 
 def index(request):
     """
-    Root site url. Displays links to all added sub-projects.
+    Root site index. Displays links to all added sub-projects.
     """
-    sub_project_sites = [
+    # Get all imported projects and related apps.
+    imported_projects = [
         app_settings['site'] for app_settings in settings.INSTALLED_CAE_PROJECTS.values() if 'site' in app_settings
     ]
+
     return render(request, 'cae_home/index.html', {
-        'sub_project_sites': sub_project_sites,
+        'imported_projects': imported_projects,
     })
