ca696468a298023a3566f53ddff367be9a52ff45
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-07-22 22:59:19 -0400

    Create basic login/logout views and associated nav links

--- cae_home/urls.py
+++ cae_home/urls.py
@@ -2,6 +2,7 @@
 Urls for CAE_Home App.
 """
 
+from django.contrib.auth import views as auth_views
 from django.conf.urls import url
 
 from . import views
@@ -9,6 +10,11 @@ from . import views
 
 app_name = 'cae_home'
 urlpatterns = [
+
+    # Auth pages.
+    url(r'^user/login/$', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    url(r'^user/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/',}),
+
     # Home page.
     url(r'^$', views.index, name='index')
 ]
