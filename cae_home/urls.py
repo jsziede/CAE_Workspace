329d3c43db5a2e2e80964ea9ec9a734450b0fc7b
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-08-03 22:55:43 -0400

    Create initial setup for "wmu clone" front end

--- cae_home/urls.py
+++ cae_home/urls.py
@@ -3,6 +3,7 @@ Urls for CAE_Home App.
 """
 
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 from django.conf.urls import url
 
 from . import views
@@ -15,6 +16,14 @@ urlpatterns = [
     url(r'^user/login/$', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     url(r'^user/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/',}),
 
-    # Home page.
-    url(r'^$', views.index, name='index')
+    # CAE Home/Index page.
+    url(r'^$', views.index, name='index'),
 ]
+
+
+# Debug only urls.
+if settings.DEBUG:
+    urlpatterns += [
+        # "WMU Clone" test page. Only use for layout and css development.
+        url(r'^wmu/$', views.wmu_test, name='wmu_test'),
+    ]
