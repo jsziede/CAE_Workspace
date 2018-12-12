3e296ccaf4baee9ecc0de07543cc7b1f8b72e3bd
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-12-12 18:03:38 -0500

    Rework seeds of users/group permissions to be callable from testing

--- cae_home/management/commands/seeders/user.py
+++ cae_home/management/commands/seeders/user.py
@@ -5,7 +5,6 @@ Seeder for "User" related Core Models.
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from faker import Faker
-from random import randint
 
 from cae_home import models
 
@@ -26,41 +25,62 @@ def create_groups():
     Creates django "auth_group" models and allocates proper permissions.
     """
     # Create base groups.
-    director_group = Group.objects.get_or_create(name='CAE Director')[0]
-    building_coordinator_group = Group.objects.get_or_create(name='CAE Building Coordinator')[0]
-    admin_ga_group = Group.objects.get_or_create(name='CAE Admin GA')[0]
-    programmer_ga_group = Group.objects.get_or_create(name='CAE Programmer GA')[0]
-    attendant_group = Group.objects.get_or_create(name='CAE Attendant')[0]
-    admin_group = Group.objects.get_or_create(name='CAE Admin')[0]
-    programmer_group = Group.objects.get_or_create(name='CAE Programmer')[0]
+    group_array = create_permission_groups()
 
     # Set director permissions. Want all, unconditionally.
-    director_group.permissions.set(Permission.objects.all())
+    group_array[0].permissions.set(Permission.objects.all())
 
     # Set building coordinator permissions. Want all, unconditionally.
-    building_coordinator_group.permissions.set(Permission.objects.all())
+    group_array[1].permissions.set(Permission.objects.all())
 
     # Set programmer permissions. Want all, unconditionally.
-    programmer_group.permissions.set(Permission.objects.all())
+    group_array[5].permissions.set(Permission.objects.all())
 
     # Set programmer GA permissions. Want all, unconditionally.
-    programmer_ga_group.permissions.set(Permission.objects.all())
+    group_array[3].permissions.set(Permission.objects.all())
 
     # Set admin GA permissions. Want all, unconditionally.
-    admin_ga_group.permissions.set(Permission.objects.all())
+    group_array[2].permissions.set(Permission.objects.all())
 
     # Set admin permissions. Want only the ones directly related to the CAE Center.
     cae_center_permissions = get_cae_center_permissions()
-    admin_group.permissions.set(cae_center_permissions)
+    group_array[4].permissions.set(cae_center_permissions)
 
     # Set attendant permissions. Want only CAE Center add privileges.
     filtered_permissions = []
     for permission in cae_center_permissions:
         if 'Can add' in permission.name or 'Can change user' in permission.name:
             filtered_permissions.append(permission)
-    attendant_group.permissions.set(filtered_permissions)
+    group_array[6].permissions.set(filtered_permissions)
 
     print('Populated group models.')
+
+
+def create_permission_groups():
+    """
+    Create main permission groups.
+    :return: Array of permission groups.
+    """
+    # Create permission groups.
+    director_group = Group.objects.get_or_create(name='CAE Director')[0]
+    building_coordinator_group = Group.objects.get_or_create(name='CAE Building Coordinator')[0]
+    admin_ga_group = Group.objects.get_or_create(name='CAE Admin GA')[0]
+    programmer_ga_group = Group.objects.get_or_create(name='CAE Programmer GA')[0]
+    admin_group = Group.objects.get_or_create(name='CAE Admin')[0]
+    programmer_group = Group.objects.get_or_create(name='CAE Programmer')[0]
+    attendant_group = Group.objects.get_or_create(name='CAE Attendant')[0]
+
+    # Create and add to array. Used in testing.
+    group_array = []                                # Index Num:
+    group_array.append(director_group)              # 0
+    group_array.append(building_coordinator_group)  # 1
+    group_array.append(admin_ga_group)              # 2
+    group_array.append(programmer_ga_group)         # 3
+    group_array.append(admin_group)                 # 4
+    group_array.append(programmer_group)            # 5
+    group_array.append(attendant_group)             # 6
+    return group_array
+
 
 
 def get_cae_center_permissions():
@@ -107,6 +127,17 @@ def create_users():
     models.User.get_or_create_superuser('skd6970', '', default_password)  # Steven (Senior Design)
     models.User.get_or_create_superuser('jbn6294', '', default_password)  # Josh (Senior Design)
 
+    create_permission_group_users(default_password=default_password)
+
+    print('Populated user models.')
+
+
+def create_permission_group_users(default_password='temppass2'):
+    """
+    Create specific users for each main group permission.
+    :param default_password: Default password to use.
+    :return: Array of users.
+    """
     # Create normal users for every main permission group.
     cae_director = models.User.get_or_create_user('cae_director', '', default_password)
     cae_building_coordinator = models.User.get_or_create_user('cae_building_coordinator', '', default_password)
@@ -114,6 +145,7 @@ def create_users():
     cae_programmer_ga = models.User.get_or_create_user('cae_programmer_ga', '', default_password)
     cae_admin = models.User.get_or_create_user('cae_admin', '', default_password)
     cae_programmer = models.User.get_or_create_user('cae_programmer', '', default_password)
+    cae_attendant = models.User.get_or_create_user('cae_attendant', '', default_password)
 
     # Add permission groups to users.
     cae_director.groups.add(Group.objects.get(name='CAE Director'))
@@ -123,7 +155,16 @@ def create_users():
     cae_admin.groups.add(Group.objects.get(name='CAE Admin'))
     cae_programmer.groups.add(Group.objects.get(name='CAE Programmer'))
 
-    print('Populated user models.')
+    # Create and add to array. Used in testing.
+    user_array = []                                 # Index Num:
+    user_array.append(cae_director)                 # 0
+    user_array.append(cae_building_coordinator)     # 1
+    user_array.append(cae_admin_ga)                 # 2
+    user_array.append(cae_programmer_ga)            # 3
+    user_array.append(cae_admin)                    # 4
+    user_array.append(cae_programmer)               # 5
+    user_array.append(cae_attendant)                # 6
+    return user_array
 
 
 def create_addresses(model_count):
