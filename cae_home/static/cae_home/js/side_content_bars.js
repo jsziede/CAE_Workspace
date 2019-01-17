7849ccadae3d219b14d31962e230f7504af21c92
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-01-17 13:49:35 -0500

    Clean up content bar logic

--- cae_home/static/cae_home/js/side_content_bars.js
+++ cae_home/static/cae_home/js/side_content_bars.js
@@ -1,48 +1,49 @@
 /**
- *  Handles removal of empty content bars.
+ *  Handles displaying and removal of side content bars.
+ *
+ *  Note that content bars default to hidden. That way, if JS is broken, they simply don't show.
+ *  This also prevents visual bug of content bars displaying for half a second on page load,
+ *  before JS can process.
  */
 
 
 // Wait for full page load.
 $(document).ready(function() {
-    console.log('Started side_content_bars.js file.');
-
-    var left_bar = $('#content-bar-left');
-    var right_bar = $('#content-bar-right');
-    console.log(left_bar);
-    console.log(right_bar);
+    // console.log('Started side_content_bars.js file.');
 
     // Check if content bars have any elements. Remove if not. Hide if they do.
-    if (left_bar.children().length == 0) {
-        left_bar.remove();
+    if ($('#content-bar-left .bar-content').children().length == 0) {
+        $('#content-bar-left').remove();
     } else {
+        $('#content-bar-left').removeClass('hidden');
         hide_left_bar();
     }
-    if (right_bar.children().length == 0) {
-        right_bar.remove();
+    if ($('#content-bar-right .bar-content').children().length == 0) {
+        $('#content-bar-right').remove();
     } else {
+        $('#content-bar-right').removeClass('hidden');
         hide_right_bar();
     }
 
     // Handle menu clicks.
     // Hide left bar on left arrow click.
     $('#content-bar-left .arrow-left').on('click', function() {
-        console.log('Clicked left arrow.');
+        // console.log('Clicked left arrow.');
         hide_left_bar();
     });
     // Show left bar on right arrow click.
     $('#content-bar-left .arrow-right').on('click', function() {
-        console.log('Clicked right arrow.');
+        // console.log('Clicked right arrow.');
         show_left_bar()
     });
     // Hide right bar on right arrow click.
     $('#content-bar-right .arrow-right').on('click', function() {
-        console.log('Clicked left arrow.');
+        // console.log('Clicked left arrow.');
         hide_right_bar();
     });
     // Show right bar on left arrow click.
     $('#content-bar-right .arrow-left').on('click', function() {
-        console.log('Clicked right arrow.');
+        // console.log('Clicked right arrow.');
         show_right_bar()
     });
 
