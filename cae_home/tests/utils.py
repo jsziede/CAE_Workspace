a6d41c34a9a15729950db7a145e283b03476e0f8
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-04-11 00:28:04 -0400

    Add documentation to tests/utils.py and refactor some methods

--- cae_home/tests/utils.py
+++ cae_home/tests/utils.py
@@ -1,10 +1,14 @@
-"""CAE Home Test utility functions and classes"""
+"""
+CAE Home test utility functions and classes.
+"""
+
 import sys
 
 from channels.testing import ChannelsLiveServerTestCase
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Group, Permission
+from django.db.models import ObjectDoesNotExist
 from django.http import QueryDict
 from django.test import TestCase
 from django.urls import reverse
@@ -16,6 +20,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 from .. import models
+from cae_home.management.commands.seeders.user import create_groups, create_permission_group_users
 
 
 UserModel = get_user_model()  # pylint: disable=invalid-name
@@ -53,28 +58,98 @@ def create_room(room_type, name, slug, **kwargs):
 
 
 class IntegrationTestCase(TestCase):
-    """Test without Selenium"""
+    """
+    Python Unit Testing extension (without Selenium).
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._debug_print = False
 
     def setUp(self):
+        """
+        Logic to reset state before each individual test.
+        """
         super().setUp()
-        self.user = UserModel.objects.create_user(
-            username='steven', password='test')
 
-    def addPermission(self, name):
-        self.user.user_permissions.add(Permission.objects.get(codename=name))
+        # Create expected groups and dummy users to associate with them.
+        create_groups()
+        create_permission_group_users(with_names=False)
 
-    # Adapted from django.tests.auth_tests.test_views.AuthViewsTestCase
+    def create_user(self, username, password, permissions=None, groups=None):
+        """
+        Create new user. Optionally pass permissions/groups.
+        :param username: Username to use.
+        :param password: Password for user.
+        :param permissions: Optional permissions to add.
+        :param groups: Optional permission groups to add.
+        :return: Instance of created user.
+        """
+        # Create user.
+        user = UserModel.objects.create_user(username=username, password=password)
+        user.password_string = password
+
+        # Check for optional permissions.
+        if permissions:
+            if isinstance(permissions, list) or isinstance(permissions, tuple):
+                for permission in permissions:
+                    self.add_permission(user, permission)
+            else:
+                self.add_permission(user, permissions)
+
+        # Check for optional groups.
+        if groups:
+            if isinstance(groups, list) or isinstance(groups, tuple):
+                for group in groups:
+                    self.add_group(user, group)
+            else:
+                self.add_group(user, groups)
+
+        return user
+
+    def add_permission(self, user, name):
+        """
+        Add a permission to the given user.
+        Ex: 'change_order'
+        On failure, prints out all possible permissions and moves to next test.
+        :param user: User object to add permission to.
+        :param name: Permission name to add.
+        """
+        try:
+            # Add permission.
+            permission = Permission.objects.get(codename=name)
+            user.user_permissions.add(permission)
+        except ObjectDoesNotExist:
+            # Failed to find permission.
+            print(list(Permission.objects.all().values_list('codename')))
+            raise ObjectDoesNotExist('Permission matching {0} not found.'.format(name))
+
+    def add_group(self, user, name):
+        """
+        Add a permission group to the given user.
+        Ex: 'CAE Admin'
+        :param user: User object to add permission to.
+        :param name: Group name to add.
+        """
+        try:
+            # Add group.
+            group = Group.objects.get(name=name)
+            user.groups.add(group)
+        except ObjectDoesNotExist:
+            # Failed to find group.
+            print(list(Group.objects.all().values_list('name')))
+            raise ObjectDoesNotExist('Group matching {0} was not found.'.format(name))
+
     def assertURLEqual(self, url, expected, parse_qs=False):
         """
         Given two URLs, make sure all their components (the ones given by
         urlparse) are equal, only comparing components that are present in both
         URLs.
+
         If `parse_qs` is True, then the querystrings are parsed with QueryDict.
         This is useful if you don't want the order of parameters to matter.
         Otherwise, the query strings are compared as-is.
+
+        Adapted from django.tests.auth_tests.test_views.AuthViewsTestCase
         """
         fields = ParseResult._fields
 
@@ -193,9 +268,16 @@ class LiveServerTestCase(ChannelsLiveServerTestCase):
     serve_static = True
     NUM_DRIVERS = 1
 
+
+    #region Class Setup and Teardown
+
     @classmethod
     def setUpClass(cls):
+        """
+        Logic to run once, before any tests.
+        """
         super().setUpClass()
+
         try:
             # NOTE: Requires "chromedriver" binary to be installed in $PATH
             # https://sites.google.com/a/chromium.org/chromedriver/getting-started
@@ -218,70 +300,187 @@ class LiveServerTestCase(ChannelsLiveServerTestCase):
 
     @classmethod
     def tearDownClass(cls):
+        """
+        Logic to run once, after all tests.
+        """
         # Quit all browser windows
         for driver in cls._drivers:
             driver.quit()
         super().tearDownClass()
 
     def setUp(self):
+        """
+        Logic to reset state before each individual test.
+        """
         super().setUp()
-        self.password1 = 'password'
-        self.user1 = UserModel.objects.create_user(
-            username='user1', password=self.password1)
-        self.password2 = self.password1
-        self.user2 = UserModel.objects.create_user(
-            username='user2', password=self.password2)
+
+        # Create expected groups and dummy users to associate with them.
+        create_groups()
+        create_permission_group_users(with_names=False)
 
     def tearDown(self):
+        """
+        Logic to reset state after each individual test.
+        """
         super().tearDown()
+
         # Close all windows
         for driver in self._drivers:
             self._close_all_new_windows(driver)
 
-    # Helper functions
-    def addPermission(self, user, name):
-        """Add a permission to the given user. E.g. 'change_order'"""
-        perm = Permission.objects.filter(codename=name).first()
+    #endregion Class Setup and Teardown
 
-        if not perm:
-            print(list(Permission.objects.all().values_list('codename')))
-            raise Exception("Permission matching {} not found".format(name))
 
-        user.user_permissions.add(perm)
+    #region Helper Functions
 
-    def _wait_for_id(self, driver, element_id, msg=None):
-        return self._do_wait(driver, By.ID, element_id, msg)
+    #region User Management Helper Functions
 
-    def _wait_for_css(self, driver, element_css, msg=None):
-        return self._do_wait(driver, By.CSS_SELECTOR, element_css, msg)
+    def create_user(self, username, password, permissions=None, groups=None):
+        """
+        Create new user. Optionally pass permissions/groups.
+        :param username: Username to use.
+        :param password: Password for user.
+        :param permissions: Optional permissions to add.
+        :param groups: Optional permission groups to add.
+        :return: Instance of created user.
+        """
+        # Create user.
+        user = UserModel.objects.create_user(username=username, password=password)
+        user.password_string = password
 
-    def _wait_for_xpath(self, driver, element_xpath, msg=None):
-        return self._do_wait(driver, By.XPATH, element_xpath, msg)
+        # Check for optional permissions.
+        if permissions:
+            if isinstance(permissions, list) or isinstance(permissions, tuple):
+                for permission in permissions:
+                    self.add_permission(user, permission)
+            else:
+                self.add_permission(user, permissions)
 
-    def _do_wait(self, driver, by, query, msg):
-        """Show an error message if wait times out"""
+        # Check for optional groups.
+        if groups:
+            if isinstance(groups, list) or isinstance(groups, tuple):
+                for group in groups:
+                    self.add_group(user, group)
+            else:
+                self.add_group(user, groups)
+
+        return user
+
+    def add_permission(self, user, name):
+        """
+        Add a permission to the given user.
+        Ex: 'change_order'
+        On failure, prints out all possible permissions and moves to next test.
+        :param user: User object to add permission to.
+        :param name: Permission name to add.
+        """
         try:
-            WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((by, query)),
-            )
-        except TimeoutException:
-            self.fail(msg or "Element not found within time limit.")
+            # Add permission.
+            permission = Permission.objects.get(codename=name)
+            user.user_permissions.add(permission)
+        except ObjectDoesNotExist:
+            # Failed to find permission.
+            print(list(Permission.objects.all().values_list('codename')))
+            raise ObjectDoesNotExist('Permission matching {0} not found.'.format(name))
+
+    def add_group(self, user, name):
+        """
+        Add a permission group to the given user.
+        Ex: 'CAE Admin'
+        :param user: User object to add permission to.
+        :param name: Group name to add.
+        """
+        try:
+            # Add group.
+            group = Group.objects.get(name=name)
+            user.groups.add(group)
+        except ObjectDoesNotExist:
+            # Failed to find group.
+            print(list(Group.objects.all().values_list('name')))
+            raise ObjectDoesNotExist('Group matching {0} was not found.'.format(name))
 
     def _login(self, driver, username, password):
+        """
+        Attempt to login on given browser window with given user.
+        :param driver: Browser manager instance to login on.
+        :param username: Username to login with.
+        :param password: Password associated with user.
+        """
+        # Get proper login url.
         login_url = settings.LOGIN_URL
         if not login_url.startswith('/'):
-            # Might be a named url pattern
+            # Might be a named url pattern.
             login_url = reverse(login_url)
+
+        # Attempt login.
         driver.get(self.live_server_url + login_url)
         driver.find_element_by_name('username').send_keys(username)
         driver.find_element_by_name('password').send_keys(password)
         driver.find_element_by_css_selector('[type="submit"]').click()
 
+    #endregion User Management Helper Functions
+
+    #region Wait Helper Functions
+
+    def _wait_for_id(self, driver, element_id, msg=None, wait_time=10):
+        """
+        Wait for provided css id to show on page.
+        :param driver: Browser manager instance to wait on.
+        :param element_id: Css element to wait for.
+        :param msg: Optional message to display on failure.
+        :param wait_time: Time to wait. Defaults to 10 seconds.
+        """
+        return self.__do_wait(driver, By.ID, element_id, msg, wait_time)
+
+    def _wait_for_css(self, driver, element_css, msg=None, wait_time=10):
+        """
+        Wait for provided css class to show on page.
+        :param driver: Browser manager instance to wait on.
+        :param element_css: Css element to wait for.
+        :param msg: Optional message to display on failure.
+        :param wait_time: Time to wait. Defaults to 10 seconds.
+        """
+        return self.__do_wait(driver, By.CSS_SELECTOR, element_css, msg, wait_time)
+
+    def _wait_for_xpath(self, driver, element_xpath, msg=None, wait_time=10):
+        """
+        Wait for xpath (xml?) to show on page.
+        :param driver: Browser manager instance to wait on.
+        :param element_xpath: Xpath element to wait for.
+        :param msg: Optional message to display on failure.
+        :param wait_time: Time to wait. Defaults to 10 seconds.
+        """
+        return self.__do_wait(driver, By.XPATH, element_xpath, msg, wait_time)
+
+    def __do_wait(self, driver, by, query, msg, wait_time):
+        """
+        Attempt to wait for given value to show on page.
+        After time expired, display fail message and quit test.
+        """
+        try:
+            WebDriverWait(driver, wait_time).until(
+                EC.visibility_of_element_located((by, query)),
+            )
+        except TimeoutException:
+            self.fail(msg or "Element not found within time limit.")
+
+    #endregion Wait Helper Functions
+
+    #region Window Manipulation Helper functions
+
     def _open_new_window(self, driver):
+        """
+        Open a new window under the given driver.
+        :param driver: Driver to open window under.
+        """
         driver.execute_script('window.open("about:blank", "_blank");')
         driver.switch_to.window(driver.window_handles[-1])
 
     def _close_all_new_windows(self, driver):
+        """
+        Close additional windows under the given driver.
+        :param driver: Driver to close additional windows of.
+        """
         while len(driver.window_handles) > 1:
             driver.switch_to.window(driver.window_handles[-1])
             driver.execute_script('window.close();')
@@ -289,4 +488,14 @@ class LiveServerTestCase(ChannelsLiveServerTestCase):
             driver.switch_to.window(driver.window_handles[0])
 
     def _switch_to_window(self, driver, window_index):
+        """
+        Switch to window held by given driver.
+        :param driver: Driver to switch window of.
+        :param window_index: Index of window under driver.
+        """
         driver.switch_to.window(driver.window_handles[window_index])
+
+
+    #endregion Window Manipulation Helper Functions
+
+    #endregion Helper Functions
